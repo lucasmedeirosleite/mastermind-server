b98923341f7dbac3557240c16e5d4e1940602082
Lucas Medeiros Leite <lucastoc@gmail.com>
2014-08-04 09:05:06 -0300

    adding routers

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -3,6 +3,7 @@ require "mongoid"
 require "redis"
 
 Dir["#{File.dirname(__FILE__)}/mastermind/config/initializers/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/mastermind/config/routers/*.rb"].each { |f| require f }
 
 require "mastermind/version"
 
