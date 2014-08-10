4836b7a7a06411eb3fc4c6e05b9a5ba6ef33784c
Lucas Medeiros Leite <lucastoc@gmail.com>
2014-08-10 20:43:32 -0300

    adding message validation

--- lib/mastermind/app/models/message.rb
+++ lib/mastermind/app/models/message.rb
@@ -7,6 +7,10 @@ class Mastermind::Message
 	embeds_one :from, class_name: 'Mastermind::Player'
 	embeds_one :to, class_name: 'Mastermind::Player'
 
+	validates_presence_of :text, message: "Text é obrigatório"
+	validates_presence_of :from, message: "Remetente é obrigatório"
+	validates_presence_of :to, message: "Destinatário é obrigatório"
+
 	embedded_in :chat, class_name: 'Mastermind::Chat' 
 
 end
