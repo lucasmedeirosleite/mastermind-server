ab25cace1c2368b75f126b4456e45eeed33d59d8
Lucas Medeiros Leite <lucastoc@gmail.com>
2014-08-10 16:20:51 -0300

    adding player class and its validations

---
+++ lib/mastermind/app/models/player.rb
@@ -0,0 +1,10 @@
+class Mastermind::Player
+	include Mongoid::Document
+	include ActiveModel::Validations
+	
+	field :nickname, type: String
+
+	validates_presence_of :nickname, message: 'Seu apelido é obrigatório'
+	validates_uniqueness_of :nickname, message: 'Já existe um jogador com este apelido'
+
+end
