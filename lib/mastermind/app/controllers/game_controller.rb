b9bbd4c33ce923cc1ae7aaa6eaee718a2e62c986
Lucas Medeiros Leite <lucastoc@gmail.com>
2014-08-19 00:27:33 -0300

    adding finish game call

--- lib/mastermind/app/controllers/game_controller.rb
+++ lib/mastermind/app/controllers/game_controller.rb
@@ -1,10 +1,17 @@
 class Mastermind::GameController
 
 	def start(params = {})
-		challenger = Mastermind::Player.find(params[:challenger_id])
-		guesser = Mastermind::Player.find(params[:guesser_id])
-		game = Mastermind::Game.new(challenger: challenger, guesser: guesser)
-		if game.save
+		game = Mastermind::Game.to_guesses_and_challenger(params)
+		if game.start
+			Nexus::Response.new(body: game)
+		else
+			Nexus::Response.new(body: game, success: false)
+		end
+	end
+
+	def end(params = {})
+		game = Mastermind::Game.find(params[:id])
+		if game.end
 			Nexus::Response.new(body: game)
 		else
 			Nexus::Response.new(body: game, success: false)
--- lib/mastermind/app/models/game.rb
+++ lib/mastermind/app/models/game.rb
@@ -3,6 +3,7 @@ class Mastermind::Game
 	include Mongoid::Document
 	include ActiveModel::Validations
 
+	field :finished, type: Boolean
 	embeds_one :challenger, class_name: 'Mastermind::Player'
 	embeds_one :guesser, class_name: 'Mastermind::Player'
 	embeds_one :chat, class_name: 'Mastermind::Chat', cascade_callbacks: true	
@@ -11,6 +12,25 @@ class Mastermind::Game
 	validates_presence_of :guesser, message: 'Adivinhador é obrigatório'
 
 	before_create :create_chat
+
+	def started?
+		not finished?
+	end
+
+	def self.to_guesses_and_challenger(options = {})
+		challenger = Mastermind::Player.find(options[:challenger_id])
+		guesser = Mastermind::Player.find(options[:guesser_id])
+		Mastermind::Game.new(challenger: challenger, guesser: guesser)
+	end
+
+	def start
+		self.save
+	end
+
+	def end
+		self.finished = true
+		self.save!
+	end
 
 	def create_chat
 		self.chat = Mastermind::Chat.new
